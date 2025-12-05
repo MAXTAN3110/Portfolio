9f921aeeb35eff5fddd75fc233c302ea358e06b8
MAXTAN3110 <shen9003@gmail.com>
2025-12-05 11:03:12 +0800

    fix import error

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ import os
 # make sure /src is in the path so imports work on Vercel
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
 
-from src.app import app as dash_app
+from app import app as dash_app
 
 # Vercel exposes this Flask server
 app = dash_app.server
